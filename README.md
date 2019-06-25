8193287d23c48b9aefe889de9f2a714b6b025830
Neil Coggins <neil_coggins@hotmail.com>
2019-06-25 22:39:39 +0100

    added build script; updated README.md; removed extraneous emacs save files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # haven-patches
+
+This repository is a test-bed to attempt to perform a full rebase of the latest monero code
+(currently v0.14.1.0) using a series of patch files.
+
+To build haven, simply:
+
+1. run the "build-haven.sh" script
+
+2. enjoy!
---
+++ build-haven.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check for presence of monero code
+if [ ! -d "monero" ] || [ ! -e "monero/.git" ]; then
+
+    # Check out monero code
+    echo "Monero tree not found in $PWD - cloning from github..."
+    git clone --recursive https://github.com/monero-project/monero
+fi
+
+# Reset monero code to HEAD
+pushd monero
+git checkout -b v0.14.1.0
+git reset HEAD --hard
+popd
+
+# Apply patches / whole files to the monero codebase
+find src -type f | while read line ; do
+    if [[ $line =~ ".git/" ]]; then
+        continue
+    elif [[ $line =~ "^README.md$" ]]; then
+        continue
+    fi
+    if [[ ${line: -6} == ".patch" ]]; then
+        patchfile=$line
+        filename=${patchfile//\.patch/}
+        dstfilename="monero/$filename"
+        #echo "Applying patch file $patchfile for target $dstfilename ...";
+        patch -p0 $dstfilename < $patchfile
+    else
+        dstfilename="monero/$line"
+        echo "Copying file $line to $dstfilename ...";
+        cp $line $dstfilename
+    fi
+done
