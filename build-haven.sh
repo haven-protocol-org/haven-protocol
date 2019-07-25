ae4d38d6a584794a332c0e8e1d7a6031a9f37b74
Neil Coggins <ncoggins@jupiter.localdomain>
2019-07-25 12:32:30 +0100

    initial import of modified DB schema for offshore

--- build-haven.sh
+++ build-haven.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+MONERO_BRANCH="release-v0.14"
+
 # Check for presence of monero code
 if [ ! -d "monero" ] || [ ! -e "monero/.git" ]; then
 
     # Check out monero code
     echo "Monero tree not found in $PWD - cloning from github..."
-    git clone --recursive https://github.com/monero-project/monero
+    git clone -b $MONERO_BRANCH --recursive https://github.com/monero-project/monero
+    pushd monero > /dev/null 2>&1
+    git branch --set-upstream-to=origin/$MONERO_BRANCH $MONERO_BRANCH
+    popd > /dev/null 2>&1
 fi
 
 # Reset monero code to HEAD
 pushd monero > /dev/null 2>&1
-git checkout -b master
+git checkout -b $MONERO_BRANCH
 git reset HEAD --hard
 git pull -t
 popd > /dev/null 2>&1
@@ -32,7 +37,11 @@ find src -type f | while read line ; do
         patch -p0 $dstfilename < $patchfile
     else
         dstfilename="monero/$line"
+	foldername=${line%/*}
         echo "Copying file $line to $dstfilename ...";
+	if [[ ! -d "monero/$foldername" ]]; then
+	    mkdir -p "monero/$foldername"
+	fi
         cp $line $dstfilename
     fi
 done
